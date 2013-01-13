0e3d4843ca5f29f46a6186b46f910a36a7dd44fc
Smit Patel <smit1293@gmail.com>
2013-01-12 20:52:02 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smitbook::Application.config.secret_token = '87e7c4ae4980d9caa386991c8faa7e77763b321a5ce4fd1175ccf67f91dfe961f0c686fdd94969a1586ab99f7e2d60e116223261b3b8c91674a8bae6250e7db1'
